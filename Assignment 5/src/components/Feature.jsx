b030990ec26e87147973d360ce1798b98d07a8e7
Roy Jiang <roy.jiang@student.tdsb.on.ca>
2024-12-03 10:44:30 -0500

    added api key

--- Assignment 5/src/components/Feature.jsx
+++ Assignment 5/src/components/Feature.jsx
@@ -1,2 +1,5 @@
-import { useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Feature.css";
 
